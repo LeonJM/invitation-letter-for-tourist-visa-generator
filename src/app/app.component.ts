be7b8cbbaf2e51b452eb9e7899caa7a39a783977
Maleon Johan-Mosi <leon.jm@outlook.com>
2025-08-31 19:09:18 +1000

    Remove SSR. Install html2pdf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'invitation-letter-for-tourist-visa-generator';
+
+  constructor() {
+
+  }
+
+  generatePdf(): void {
+    const element = document.getElementById('pdf-content');
+
+    if (element) {
+      const opt = {
+        margin:       0.5,
+        filename:     'my-document.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+      };
+
+      html2pdf().set(opt).from(element).save();
+    }
+
+  }
 }
+
